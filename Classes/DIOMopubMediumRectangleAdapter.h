4c68145426b87b3db01831753385fa9b264db595
Ariel Malka <ariel@chronotext.org>
2019-12-26 18:57:24 +0200

    Updating classes

---
+++ Classes/DIOMopubMediumRectangleAdapter.h
@@ -0,0 +1,26 @@
+//
+//  DIOMopubMediumRectangleAdapter.h
+//  MopubAdapterForiOS
+//
+//  Created by rdorofeev on 17.12.2019.
+//  Copyright © 2019 rdorofeev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#if __has_include(<MoPub/MoPub.h>)
+#import <MoPub/MoPub.h>
+#elif __has_include(<MoPubSDKFramework/MoPub.h>)
+#import <MoPubSDKFramework/MoPub.h>
+#else
+#import "MPBannerCustomEvent.h"
+#endif
+
+#import <DIOSDK/DIOController.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DIOMopubMediumRectangleAdapter : MPBannerCustomEvent
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Classes/DIOMopubMediumRectangleAdapter.m
@@ -0,0 +1,67 @@
+
+//  DIOMopubMediumRectangleAdapter.m
+//  MopubAdapterForiOS
+//
+//  Created by rdorofeev on 17.12.2019.
+//  Copyright © 2019 rdorofeev. All rights reserved.
+//
+
+#import "DIOMopubMediumRectangleAdapter.h"
+#import <DIOSDK/DIOController.h>
+#import "MoPub.h"
+
+@interface DIOMopubMediumRectangleAdapter ()
+
+@end
+
+@implementation DIOMopubMediumRectangleAdapter
+
+- (void)requestAdWithSize:(CGSize)size customEventInfo:(NSDictionary *)info{
+    NSString *placementId = [info objectForKey:@"placementid"];
+    
+    if (![DIOController sharedInstance].initialized) {
+        NSLog(@"Error: DIOController not initialized!");
+        NSError *error = [NSError errorWithDomain:@"https://appsrv.display.io/srv"
+                                             code:100
+                                         userInfo:@{NSLocalizedDescriptionKey:@"DIOController not initialized!"}];
+        [self.delegate bannerCustomEvent:self didFailToLoadAdWithError: error];
+    }else {
+        NSLog(@"Trying to load banner for placement %@", placementId);
+        DIOConsentState state = [[MoPub sharedInstance] canCollectPersonalInfo] ? DIOConsentStateYES : DIOConsentStateNO;
+        DIOConsentState gdpr = [[MoPub sharedInstance] isGDPRApplicable] ? DIOConsentStateYES : DIOConsentStateNO;
+        [[DIOController sharedInstance] setConsentData:state gdprState:gdpr];
+        [[DIOController sharedInstance] setMediationPlatform:DIOMediationPlatformMopub];
+        
+        [self loadDioMediumRect:placementId];
+    }
+}
+
+- (void)loadDioMediumRect:(NSString *)placementId{
+    DIOPlacement *placement = [[DIOController sharedInstance] placementWithId:placementId];
+    if (!placement) {
+        NSError *error = [NSError errorWithDomain:@"https://appsrv.display.io/srv"
+                                             code:100
+                                         userInfo:@{NSLocalizedDescriptionKey:@"Invalid placement"}];
+        [self.delegate bannerCustomEvent:self didFailToLoadAdWithError: error];
+        return;
+    }
+    
+    DIOAdRequest *request = [placement newAdRequest];
+    
+    [request setDetailsRequired:YES];
+    [request requestAdWithAdReceivedHandler:^(DIOAdProvider *adProvider) {
+        [adProvider loadAdWithLoadedHandler:^(DIOAd *ad) {
+            [self.delegate bannerCustomEvent:self didLoadAd:[ad view]];
+        } failedHandler:^(NSString *message){
+            NSError *error = [NSError errorWithDomain:@"https://appsrv.display.io/srv"
+                                                 code:100
+                                             userInfo:@{NSLocalizedDescriptionKey:message}];
+            [self.delegate bannerCustomEvent:self didFailToLoadAdWithError: error];
+            NSLog(@"%@", message);
+        }];
+    } noAdHandler:^{
+        NSLog(@"No ad");
+    }];
+}
+
+@end
